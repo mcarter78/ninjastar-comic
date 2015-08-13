7b11674999fb8568243c9536e2724d26bc8d63a5
Matt Carter <sjsharksftw@gmail.com>
2015-08-13 04:57:13 +0000

    Started work to include AngularJS

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-$(document).ready(function(){
+var app = angular.module('ninjaStar', []);
 
-$('.lightbox_trigger').click(function(e){
-    e.preventDefault();
-    var image_href = $(this).attr("href");
-    if ($('#lightbox').length > 0) {
-        $('#content').html('<img src"' + image_href + '" />');
-        $('#lightbox').show();
-    } else {
-        var lightbox =
-        '<div id="lightbox">' +
-        '<button class="btn btn-default" id="close">X</button>' +
-        '<div id="content">' + 
-        '<img src="' + image_href +'" />' +
-        '</div>' +
-        '</div>';
-        $('body').append(lightbox);
-    }
-    
-});
-
-$(document).mouseup(function (event){
-    var container = $("#lightbox");
-    if (container.has(event.target).length === 0) {
-    container.hide();
-    }
-});
-
+app.controller('ninjaStarController', function($scope){
+    $scope.comics = [
+        {
+            title: "The Summons IV",
+            date : "03-22-13",
+            href : "/img/2013-03-22.jpg"
+        }    
+    ];
 });
