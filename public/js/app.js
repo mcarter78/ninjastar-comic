3ef115cfa9562162f4071bc77817e6a9445e4154
Matt Carter <sjsharksftw@gmail.com>
2015-08-13 23:35:25 +0000

    added more comics to controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,5 @@
 var app = angular.module('ninjaStar', ['ngRoute']);
 
-
-
-// Fix this code for partial routing
-
-// var routing = angular.module('routing', [
-//     'ngRoute'    
-// ]);
-
 app.config(function($routeProvider) {
     
         $routeProvider
@@ -26,35 +18,4 @@ app.config(function($routeProvider) {
             .otherwise({
                 redirectTo: '/'
             });
-//            $locationProvider.html5Mode(true);
-});
-
-app.controller('ninjaStarController', function($scope){
-    $scope.comics = [
-        {
-            title: "The Summons IV",
-            date : "03.22.2013",
-            href : "2013-03-22.jpg"
-        },
-        {
-            title: "A Trombone Interlude",
-            date : "03.15.2013",
-            href : "2013-03-15.jpg"
-        },
-        {
-            title: "The Summons III",
-            date : "03.08.2013",
-            href : "2013-03-08.jpg"
-        },
-        {
-            title: "The Summons II",
-            date : "03.01.2013",
-            href : "2013-03-01.jpg"
-        },
-        {
-            title: "The Summons I",
-            date : "02.22.2013",
-            href : "2013-02-22.jpg"
-        }
-    ];
 });
