65b1557bb91e22bf1b6ab2913d5bb87ecfd916e6
Matt Carter <sjsharksftw@gmail.com>
2015-08-13 05:24:11 +0000

    built repeating comic list with AngularJS

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ app.controller('ninjaStarController', function($scope){
     $scope.comics = [
         {
             title: "The Summons IV",
-            date : "03-22-13",
-            href : "/img/2013-03-22.jpg"
-        }    
+            date : "03.22.2013",
+            href : "2013-03-22.jpg"
+        },
+        {
+            title: "A Trombone Interlude",
+            date : "03.15.2013",
+            href : "2013-03-15.jpg"
+        },
+        {
+            title: "The Summons III",
+            date : "03.08.2013",
+            href : "2013-03-08.jpg"
+        },
+        {
+            title: "The Summons II",
+            date : "03.01.2013",
+            href : "2013-03-01.jpg"
+        },
+        {
+            title: "The Summons I",
+            date : "02.22.2013",
+            href : "2013-02-22.jpg"
+        }
     ];
 });
